47ef4a15f5133b8f47c5ee8102dd11d6ffd7c076
Nacho Saenz <ignaciosaenzb@gmail.com>
2022-10-21 16:43:25 -0300

    commit

--- slider.js
+++ slider.js
@@ -10,12 +10,12 @@ slider.insertAdjacentElement('afterbegin',sliderSectionLast);
 function Next() {
     let sliderSectionFirst = document.querySelectorAll(".slider__section")[0];
     slider.style.marginLeft = "-200%";
-    slider.style.transition = "all 0.5s";
+    slider.style.transition = "all 1s";
     setTimeout(function(){
         slider.style.transition = "none";
         slider.insertAdjacentElement("beforeend", sliderSectionFirst);
         slider.style.marginLeft = "-100%";
-        }, 500);
+        }, 1000);
 } 
 btnRight.addEventListener('click', function(){Next();});
 
@@ -23,18 +23,18 @@ function Prev() {
     let sliderSection = document.querySelectorAll(".slider__section");
     let sliderSectionLast = sliderSection[sliderSection.length -1];
     slider.style.marginLeft = "0";
-    slider.style.transition = "all 0.5s";
+    slider.style.transition = "all 1s";
     setTimeout(function(){
         slider.style.transition = "none";
         slider.insertAdjacentElement("afterbegin", sliderSectionLast);
         slider.style.marginLeft = "-100%";
-        }, 500);
+        }, 1000);
 } 
 btnLeft.addEventListener('click', function(){Prev();});
 
 setInterval(function(){
     Next();
-}, 2000)
+}, 4000)
 
 let btnMenu = document.getElementById('btn-menu');
 let menu = document.getElementById('menu');
