21a2f93bcbf7065f5d3447b9c565bbee711b0dfc
Nacho Saenz <ignaciosaenzb@gmail.com>
2022-11-10 20:05:46 -0300

    ultimoultimo

--- slider.js
+++ slider.js
@@ -1,43 +1,43 @@
 const slider = document.querySelector("#slider");
 let sliderSection = document.querySelectorAll(".slider__section");
-let sliderSectionLast = sliderSection[sliderSection.length -1];
+let sliderSectionLast = sliderSection[sliderSection.length - 1];
 
 const btnLeft = document.querySelector("#btn-left");
 const btnRight = document.querySelector("#btn-right");
 
-slider.insertAdjacentElement('afterbegin',sliderSectionLast);
+slider.insertAdjacentElement('afterbegin', sliderSectionLast);
 
 function Next() {
     let sliderSectionFirst = document.querySelectorAll(".slider__section")[0];
     slider.style.marginLeft = "-200%";
     slider.style.transition = "all 1s";
-    setTimeout(function(){
+    setTimeout(function () {
         slider.style.transition = "none";
         slider.insertAdjacentElement("beforeend", sliderSectionFirst);
         slider.style.marginLeft = "-100%";
-        }, 1000);
-} 
-btnRight.addEventListener('click', function(){Next();});
+    }, 1000);
+}
+btnRight.addEventListener('click', function () { Next(); });
 
 function Prev() {
     let sliderSection = document.querySelectorAll(".slider__section");
-    let sliderSectionLast = sliderSection[sliderSection.length -1];
+    let sliderSectionLast = sliderSection[sliderSection.length - 1];
     slider.style.marginLeft = "0";
     slider.style.transition = "all 1s";
-    setTimeout(function(){
+    setTimeout(function () {
         slider.style.transition = "none";
         slider.insertAdjacentElement("afterbegin", sliderSectionLast);
         slider.style.marginLeft = "-100%";
-        }, 1000);
-} 
-btnLeft.addEventListener('click', function(){Prev();});
+    }, 1000);
+}
+btnLeft.addEventListener('click', function () { Prev(); });
 
-setInterval(function(){
+setInterval(function () {
     Next();
 }, 4000)
 
 let btnMenu = document.getElementById('btn-menu');
 let menu = document.getElementById('menu');
-btnMenu.addEventListener('click', function(){
+btnMenu.addEventListener('click', function () {
     menu.classList.toggle('mostrar');
 });
